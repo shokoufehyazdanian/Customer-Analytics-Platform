89aed8f1662cd02446d7aeacd4aa79ead2c21a05
shokoufeh Yazdanian <shokufe.yazdanian@gmail.com>
2026-07-14 16:53:23 +0200

    Add churn prediction API with FastAPI

---
+++ api/app/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+
+from api.app.schemas import CustomerFeatures
+from api.app.model import predict_churn
+
+
+
+app = FastAPI(
+    title="Customer Churn Prediction API"
+)
+
+
+
+@app.get("/")
+def home():
+
+    return {
+        "message":
+        "Churn API is running"
+    }
+
+
+
+@app.post("/predict")
+def predict(
+    customer: CustomerFeatures
+):
+
+    features = [
+
+        customer.frequency,
+
+        customer.monetary,
+
+        customer.recency,
+
+        customer.avg_order_value
+
+    ]
+
+
+    result = predict_churn(
+        features
+    )
+
+
+    return result
---
+++ api/app/model.py
@@ -0,0 +1,24 @@
+import joblib
+
+
+MODEL_PATH = "ml/models/churn_model.pkl"
+
+
+model = joblib.load(MODEL_PATH)
+
+
+def predict_churn(features):
+
+    prediction = model.predict(
+        [features]
+    )
+
+    probability = model.predict_proba(
+        [features]
+    )[0][1]
+
+
+    return {
+        "prediction": int(prediction[0]),
+        "churn_probability": float(probability)
+    }
