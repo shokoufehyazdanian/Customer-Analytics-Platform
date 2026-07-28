e923ac7ff59311aec678820f24e865b8820a6da3
Shokoufeh Yazdanian <shokufe.yazdanian@gmail.com>
2026-07-28 21:23:44 +0200

    Update fact_orders.sql

--- customer_analytics_dbt/models/marts/fact_orders.sql
+++ customer_analytics_dbt/models/marts/fact_orders.sql
@@ -4,15 +4,20 @@ select
 
     o.customer_id,
 
+    c.customer_unique_id,
+
     o.order_status,
 
     o.purchase_date,
 
-    c.customer_state
+    o.delivery_date,
 
+    c.customer_city,
+
+    c.customer_state
 
 from {{ ref('stg_orders') }} o
 
 left join {{ ref('stg_customers') }} c
 
-on o.customer_id = c.customer_id
+    on o.customer_id = c.customer_id
