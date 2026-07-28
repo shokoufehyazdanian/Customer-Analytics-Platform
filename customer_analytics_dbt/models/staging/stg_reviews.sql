901c6d5be514463246ffd0c498bdc26c20117245
Shokoufeh Yazdanian <shokufe.yazdanian@gmail.com>
2026-07-28 20:59:30 +0200

    Update stg_reviews.sql

--- customer_analytics_dbt/models/staging/stg_reviews.sql
+++ customer_analytics_dbt/models/staging/stg_reviews.sql
@@ -1,11 +1,17 @@
 select
 
     review_id,
+
     order_id,
-    review_score,
+
+    review_score::integer as review_score,
+
     review_comment_title,
+
     review_comment_message,
-    review_creation_date,
-    review_answer_timestamp
+
+    review_creation_date::timestamp as review_created_at,
+
+    review_answer_timestamp::timestamp as review_answered_at
 
 from {{ source('ecommerce', 'olist_order_reviews_dataset') }}
