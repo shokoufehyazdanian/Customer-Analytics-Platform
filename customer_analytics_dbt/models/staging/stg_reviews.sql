063c21fcf2c6d5dc8567999d61061e9a253c5f27
Shokoufeh Yazdanian <shokufe.yazdanian@gmail.com>
2026-08-05 00:03:59 +0200

    Update stg_reviews.sql

--- customer_analytics_dbt/models/staging/stg_reviews.sql
+++ customer_analytics_dbt/models/staging/stg_reviews.sql
@@ -1,17 +1,46 @@
+with source_reviews as (
+
+    select
+
+        review_id,
+
+        order_id,
+
+        review_score::integer as review_score,
+
+        review_comment_title,
+
+        review_comment_message,
+
+        review_creation_date::timestamp as review_created_at,
+
+        review_answer_timestamp::timestamp as review_answered_at,
+
+        row_number() over (
+            partition by review_id
+            order by review_creation_date desc
+        ) as rn
+
+    from {{ source('ecommerce', 'olist_order_reviews_dataset') }}
+
+)
+
 select
 
     review_id,
 
     order_id,
 
-    review_score::integer as review_score,
+    review_score,
 
     review_comment_title,
 
     review_comment_message,
 
-    review_creation_date::timestamp as review_created_at,
+    review_created_at,
 
-    review_answer_timestamp::timestamp as review_answered_at
+    review_answered_at
 
-from {{ source('ecommerce', 'olist_order_reviews_dataset') }}
+from source_reviews
+
+where rn = 1
